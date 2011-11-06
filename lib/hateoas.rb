2b823956f2da5747eddb9c7b6ed13976fb8b0982
Steve Klabnik <steve@steveklabnik.com>
2011-11-06 12:59:34 +0200

    zomg docs

--- lib/hateoas.rb
+++ lib/hateoas.rb
@@ -1,3 +1,9 @@
+# This is the source code for HATEOAS, a gem to help
+# you build client libraries for hypertext APIs. This
+# code is NOT ready for any kind of production, and is
+# frankly terrible. It's like two hours of work. Please
+# don't hate me.
+
 require 'hateoas/version'
 require 'open-uri'
 require 'mechanize'
